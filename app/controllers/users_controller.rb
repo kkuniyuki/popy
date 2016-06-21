8108e44f92576760822610c359512c2842bddea0
kuniyuki kikuchi <kkikuchi@netprotections.co.jp>
2016-06-21 04:50:17 +0000

    code入力機能作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!, only: [:update]
 
   def new
     @user = User.new
@@ -16,20 +17,63 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     
-    # if @user.map_id = nil
+    # if @user.map_id = nil &&
     #TODO　新しいマップデータを作る
     #     @map = Map.new()
-    #TODO　新しいコレクションデータを作る
-    #     @stampcode = Stampcode.new()
     # else
     #TODO  既存のマップデータを呼び出す
     #     @map = Map.find()
 
     #TODO　既存のコレクションデータを呼び出す
     #     @stampcode = Stampcode.find()
+      stampcode_all = StampCode.where(user_id: current_user)
+      @stamp_all = Array.new
+      stampcode_all.each do |stampcode|
+        p @stamp_all << Stamp.find_by(id: stampcode.stamp_id)
+      end     
 
     # end
 
+  end
+  
+  
+  def update
+    # TODO 入力時コードのバリデーション
+    
+    code = params[:code]
+    
+    #一意性チェック
+    stampcode = StampCode.where(code: code)
+    if stampcode != nil
+      flash[:notice] = "すでにこのコードは使用されました。"
+      render :show
+      return
+    end
+      
+    # コードの中のidを取り出す
+    @code_id = code[]
+    stamp = Stamp.find_by(id: @code_id)
+    
+    #stamp_idのチェック処理
+    if stamp != nil
+      StampCode.create(code: params[:code], user_id: current_user,
+                       stamp_id: @code_id
+                       )
+      stampcode_all = StampCode.where(user_id: current_user)
+      @stamp_all = Array.new
+      stampcode_all.each do |stampcode|
+        p @stamp_all << Stamp.find_by(id: stampcode.stamp_id)
+      end     
+
+      redirect_to @stamp_all
+    else
+　    #stamp_idのが間違いの場合
+      flash[:notice] = "コードに誤りがあります。"
+      render :show
+      return
+    end
+    
+    # @stampcode_all = StampCode.where(user_id: current_user)
   end
 
   private
@@ -40,6 +84,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def authenticate_user!
+    # current_userが@userでない時はroot_pathなどにリダイレクト
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end  
   end
     
     
